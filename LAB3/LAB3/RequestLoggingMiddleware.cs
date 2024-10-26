14a22401b302aeb63fe8ff697ccbf86f2d9a0aeb
drekin34 <dmitroslobodan@gmail.com>
2024-10-26 10:39:00 +0300

    first

---
+++ LAB3/LAB3/RequestLoggingMiddleware.cs
@@ -0,0 +1,33 @@
+﻿namespace LAB3
+{
+    using Microsoft.AspNetCore.Http;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public class RequestLoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Отримання даних запиту
+            var requestUrl = context.Request.Path + context.Request.QueryString;
+            var requestTime = DateTime.UtcNow;
+            var ipAddress = context.Connection.RemoteIpAddress?.ToString();
+
+            // Формування рядка для логування
+            var logEntry = $"{requestTime:yyyy-MM-dd HH:mm:ss} | {requestUrl} | IP: {ipAddress}";
+
+            // Запис логів у файл
+            await File.AppendAllTextAsync("requests.log", logEntry + Environment.NewLine);
+
+            // Продовжуємо обробку запиту
+            await _next(context);
+        }
+    }
+}
